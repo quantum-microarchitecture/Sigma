9b710ccb5f3401fe78530aeba1058f5c61e8468d
Savitar1995 <zq953100@mail.ustc.edu.cn>
2019-06-20 17:15:02 +0800

    Update
    1. Add the SUB instruction
    2. Modify the width of imm for load/store and branch instruction
    3. remove  bypass mode when the system is not trigger

--- sigma_QPU/QPU/QPU_exu.v
+++ sigma_QPU/QPU/QPU_exu.v
@@ -13,7 +13,7 @@
 module QPU_exu(
 
   output exu_active,
-  input  i_tragger,
+  input  i_trigger,
   //////////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////
   // The IFU IR stage to EXU interface
@@ -73,11 +73,11 @@ module QPU_exu(
   input mcu_i_wen,
   ///////////////////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////
-  ///data to tragger
-  output tragger_o_clk_ena,
-  input  [`QPU_TIME_WIDTH - 1 : 0] tragger_o_clk,
-  output [`QPU_EVENT_WIRE_WIDTH - 1 : 0] tragger_o_data,
-  output [`QPU_EVENT_NUM - 1: 0] tragger_o_valid,
+  ///data to trigger
+  output trigger_o_clk_ena,
+  input  [`QPU_TIME_WIDTH - 1 : 0] trigger_o_clk,
+  output [`QPU_EVENT_WIRE_WIDTH - 1 : 0] trigger_o_data,
+  output [`QPU_EVENT_NUM - 1: 0] trigger_o_valid,
 
   input  clk,
   input  rst_n
@@ -175,17 +175,17 @@ module QPU_exu(
     .tiq_dest_i_ready            (tiq_wbck_ready    ),
     .tiq_dest_i_data             (tiq_wbck_data     ),
 
-    .i_tragger                   (i_tragger         ),
-    .tragger_o_clk_ena           (tragger_o_clk_ena ),
-    .tragger_i_clk               (tragger_o_clk     ),
+    .i_trigger                   (i_trigger         ),
+    .trigger_o_clk_ena           (trigger_o_clk_ena ),
+    .trigger_i_clk               (trigger_o_clk     ),
 
     .evq_dest_wen                (evq_wbck_ena      ),
     .evq_dest_i_ready            (evq_wbck_ready    ),
     .evq_dest_oprand             (erf_oprand        ),      
     .evq_dest_data               (erf_data          ),
 
-    .evq_dest_o_valid            (tragger_o_valid   ),
-    .evq_dest_o_data             (tragger_o_data    ),
+    .evq_dest_o_valid            (trigger_o_valid   ),
+    .evq_dest_o_data             (trigger_o_data    ),
 
     .qubit_measure_zero          (qubit_measure_zero),
     .qubit_measure_one           (qubit_measure_one ),
